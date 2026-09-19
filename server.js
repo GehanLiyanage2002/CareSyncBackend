aab051b194ff7801113c5398ab33c1f9b8cbf33c
HKDHAmarasinhe2001 <iit22031@std.uwu.ac.lk>
2026-09-19 15:02:06 +0530

    feat: enhance service booking functionality and add booked slots retrieval

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // Load environment variables
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || 'development';
