b6e52e7e98a800d34269e82b31c78039e2048078
GehanLiyanage2002 <iit22002@std.uwu.ac.lk>
2026-09-22 11:18:05 +0530

    Remove deprecated scripts and unused database queries to clean up the codebase

--- check-user.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const db = require('./config/db');
-db.query("SELECT email, mobile_number FROM users WHERE email='pubudugunawarsana@gmail.com'")
-  .then(r => console.log(r.rows))
-  .catch(console.error)
-  .finally(() => process.exit());
